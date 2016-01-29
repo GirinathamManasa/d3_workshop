cfe8d4a03187b4ca295396cc92e6a2c015b4c9e8
Joshua Miles <milesjos@msu.edu>
2016-01-29 11:18:29 -0500

    Added some more styles, made a header, fixed the map height, and cleaned up a little bit of the js

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ var map = new Datamap({
 });
 
 //get the data
-var data = d3.csv('http://joshmiles.me/d3_workshop/pres_polls.csv', function(d){
+d3.csv('http://joshmiles.me/d3_workshop/pres_polls.csv', function(d){
 	return {
 		state: d.State,
 		ev: +d.ElectoralVotes,	//+ makes the variable an integer rather than a string
 		dem: +d.Dem,
 		rep: +d.Rep
 	};
-	}, function(d) {
-		console.log(d[0]);
-	});
+}, function(d) {
+	console.log(d[0]);
+});
