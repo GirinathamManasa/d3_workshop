8e578df9cc4c365ec9c4026eca249a138ad99db7
Joshua Miles <milesjos@msu.edu>
2016-01-29 17:05:05 -0500

    made the map resizable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-//get the data
+//get the data by letting d3 parse through the csv
 d3.csv('http://joshmiles.me/d3_workshop/pres_polls.csv', function(d){
 	return {
+		//rename the elements of the object to be better than the ones in the svg
 		state: d.State,
 		ev: +d.ElectoralVotes,	//+ makes the variable an integer rather than a string
 		dem: +d.Dem,
@@ -11,38 +12,44 @@ d3.csv('http://joshmiles.me/d3_workshop/pres_polls.csv', function(d){
 	var map = new Datamap({
 		scope: 'usa',
 		element: document.getElementById('container'), //get our container for our map
+		responsive: true,
 		geographyConfig: {
-			highlightOnHover: false
+			popupTemplate: function(geo, d) {
+	          return '<div class="hoverinfo"><div>'+geo.properties.name+'</div><div>'+'p'+'</div></div>';
+	        },
+			highlightOnHover: false //get rid of highlight on hover
 		}
 	});
 	for (var i = data.length - 1; i >= 0; i--) {
 		//colors list
 		var heavyRepublican = '#B50009'
 			republican = '#E8000A',
-			lightRepublican = '#CE333B',
+			lightRepublican = '#D3818C',
 			heavyDemocrat = '#001460',
 			democrat = '#042087',
 			lightDemocrat = '#293C87';
 		//st is for state
 		var st = d3.select('.' + data[i].state);
-
+		//if the percent for the democratic candidate was higher than the republican candidate ...
 		if (data[i].dem >= data[i].rep) {
-			if (data[i].ev >= 20) {
+			//... and the number of electoral votes was above 20...
+			if (data[i].ev >= 15) {
+				//... fill the state with heavyDemocrat
 				st.style('fill', heavyDemocrat);
-			} else if (data[i].ev < 20 && data[i].ev >= 10){
-				st.style('fill', democrat);
 			} else {
-				st.style('fill', lightDemocrat)
+				st.style('fill', lightDemocrat);
 			}
 		} else{
-			if (data[i].ev >= 20) {
+			if (data[i].ev >= 15) {
 				st.style('fill', heavyRepublican);
-			} else if (data[i].ev < 20 && data[i].ev >= 10){
-				st.style('fill', republican);
 			} else {
-				st.style('fill', lightRepublican)
+				st.style('fill', republican);
 			}
 		}
 	}
+	d3.select(window).on('resize', function() {
+        map.resize();
+    });
 	console.log(data[0]);
 });
+
