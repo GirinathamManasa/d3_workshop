6538b4e7d237fe06f87e3d613451fb60a49d2d96
Joshua Miles <milesjos@msu.edu>
2016-01-29 12:44:04 -0500

    Made the colors fill according to the data

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-//create the map
-var map = new Datamap({
-	scope: 'usa',
-	element: document.getElementById('container'),
-	fills: {
-		republican: '#E8000A',
-		democrat: '#042087'
-	}
-});
-
 //get the data
 d3.csv('http://joshmiles.me/d3_workshop/pres_polls.csv', function(d){
 	return {
@@ -16,6 +6,29 @@ d3.csv('http://joshmiles.me/d3_workshop/pres_polls.csv', function(d){
 		dem: +d.Dem,
 		rep: +d.Rep
 	};
-}, function(d) {
-	console.log(d[0]);
+}, function(data) {
+	//create the map
+	var map = new Datamap({
+		scope: 'usa',
+		element: document.getElementById('container'), //get our container for our map
+		fills: {					//set up the colors for each party
+			defaultFill: '#7184C7'
+		},
+		geographyConfig: {
+			highlightOnHover: false
+		}
+	});
+	for (var i = data.length - 1; i >= 0; i--) {
+		var republican = '#E8000A',
+			democrat = '#042087';
+		var st = d3.select('.' + data[i].state);
+		if (data[i].dem >= data[i].rep) {
+			st.style('fill', democrat)
+		} else{
+			st.style('fill', republican)
+		}
+		console.log(data[i].state);
+		console.log(st);
+	};
+	console.log(data[0]);
 });
