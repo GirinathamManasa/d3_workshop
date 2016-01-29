2abeacc171dbd34ab524b8026294fc92ddfa9ac5
Joshua Miles <milesjos@msu.edu>
2016-01-29 13:38:43 -0500

    Made the colors of the states adapt to the number of electoral votes for that state

--- index.js
+++ index.js
@@ -11,24 +11,38 @@ d3.csv('http://joshmiles.me/d3_workshop/pres_polls.csv', function(d){
 	var map = new Datamap({
 		scope: 'usa',
 		element: document.getElementById('container'), //get our container for our map
-		fills: {					//set up the colors for each party
-			defaultFill: '#7184C7'
-		},
 		geographyConfig: {
 			highlightOnHover: false
 		}
 	});
 	for (var i = data.length - 1; i >= 0; i--) {
-		var republican = '#E8000A',
-			democrat = '#042087';
+		//colors list
+		var heavyRepublican = '#B50009'
+			republican = '#E8000A',
+			lightRepublican = '#CE333B',
+			heavyDemocrat = '#001460',
+			democrat = '#042087',
+			lightDemocrat = '#293C87';
+		//st is for state
 		var st = d3.select('.' + data[i].state);
+
 		if (data[i].dem >= data[i].rep) {
-			st.style('fill', democrat)
+			if (data[i].ev >= 20) {
+				st.style('fill', heavyDemocrat);
+			} else if (data[i].ev < 20 && data[i].ev >= 10){
+				st.style('fill', democrat);
+			} else {
+				st.style('fill', lightDemocrat)
+			}
 		} else{
-			st.style('fill', republican)
+			if (data[i].ev >= 20) {
+				st.style('fill', heavyRepublican);
+			} else if (data[i].ev < 20 && data[i].ev >= 10){
+				st.style('fill', republican);
+			} else {
+				st.style('fill', lightRepublican)
+			}
 		}
-		console.log(data[i].state);
-		console.log(st);
-	};
+	}
 	console.log(data[0]);
 });
