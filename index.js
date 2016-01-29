da167c86bea3c783e7dd52b79f827c97d2167ad4
Joshua Miles <milesjos@msu.edu>
2016-01-28 22:44:38 -0500

    Added the svg file and gave the map some styles

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
-//creating the map
+//create the map
+var map = new Datamap({
+	scope: 'usa',
+	element: document.getElementById('container'),
+	fills: {
+		republican: '#E8000A',
+		democrat: '#042087'
+	}
+});
 
-// var svg = d3.select("#map");
-// var width = svg.style('width');
-// var height = svg.style('height');
-
-// var projection = d3.geo.albersUsa()
-//     .scale(1000)
-//     .translate([width / 2, height / 2]);
-
-// var path = d3.geo.path()
-//     .projection(projection);
-
-// d3.json("/mbostock/raw/4090846/us.json", function(error, us) {
-//   if (error) throw error;
-
-//   svg.insert("path", ".graticule")
-//       .datum(topojson.feature(us, us.objects.land))
-//       .attr("class", "land")
-//       .attr("d", path);
-
-//   svg.insert("path", ".graticule")
-//       .datum(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b && !(a.id / 1000 ^ b.id / 1000); }))
-//       .attr("class", "county-boundary")
-//       .attr("d", path);
-
-//   svg.insert("path", ".graticule")
-//       .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-//       .attr("class", "state-boundary")
-//       .attr("d", path);
-// })
-
+//get the data
+var data = d3.csv('pres_polls.csv', function(d){
+	return {
+		state: d.State,
+		ev: +d.ElectoralVotes,	//+ makes the variable an integer rather than a string
+		dem: +d.Dem,
+		rep: +d.Rep
+	}
+});
